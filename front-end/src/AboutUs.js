ff2faa0cd19c81c77c0c0ac1682068e3646db382
vernairesl <rl4385@nyu.edu>
2025-02-24 03:48:12 -0500

    updates

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -6,6 +6,8 @@ const AboutUs = () => {
       <h1>Hi!</h1>
       <p>My name is Sandy! I'm a student who loves animals and sports! </p>
       <p>I am also a lazyass so I decided to leave this page undecorated. </p>
+      <p>Anyway, here is a photo of me</p>
+      <p> </p>
       <img src="http://localhost:5002/images/photoOfMyself.jpg" alt="picOfMe" />
     </div>
   );
